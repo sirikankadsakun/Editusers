d7b682db0327cc6eb21da52018d32860efb3020a
kontawan <psgone2028@gmail.com>
2018-12-17 11:17:27 +0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
---
+++ src/netlog.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import './netlog.css';
+import Appheader from './Components/Appheader';
+
+class Netlog extends Component {
+    state = { }
+    render() {
+        return(
+            <div>
+                <Appheader />
+            <h1>Net Log Application</h1>
+            <h3>Created by Mr.Kontawan Jina</h3>
+                      </div>
+
+        );
+    }
+}
+
+export default Netlog;
