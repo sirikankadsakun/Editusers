07de5c01fb4fc9e1d919356f720320f9e791221e
kontawan <psgone2028@gmail.com>
2018-12-24 16:08:28 +0700

    kontawan

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import Netlog from './netlog';
 import * as serviceWorker from './serviceWorker';
 
--- src/netlog.jsx
+++ src/netlog.jsx
@@ -1,19 +1,19 @@
 import React,{Component} from 'react';
 import './netlog.css';
 import Appheader from './Components/Appheader';
-
+import Applogin from './Components/Applogin';
 class Netlog extends Component {
     state = { }
     render() {
         return(
             <div>
                 <Appheader />
-            <h1>Net Log Application</h1>
-            <h3>Created by Mr.Kontawan Jina</h3>
+            <Applogin />
                       </div>
 
         );
     }
+    
 }
 
 export default Netlog;
